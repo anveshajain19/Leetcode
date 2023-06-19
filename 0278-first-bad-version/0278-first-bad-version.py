558f8090bbd5b93aca614f6ac37f76308cd7ea0a
Anvesha19 <92093714+anveshajain19@users.noreply.github.com>
2023-06-19 14:46:43 +0530

    Time: 55 ms (5.97%), Space: 16.3 MB (56.80%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        r = n-1
+        l = 0
+        while(l<=r):
+            mid = l + (r-l)//2
+            if isBadVersion(mid)==False:
+                l = mid+1
+            else:
+                r = mid-1
+        return l
+       
