9cabc50468db09acdca72145358657506733f5c0
Anvesha19 <92093714+anveshajain19@users.noreply.github.com>
2024-06-17 12:59:16 +0530

    Time: 74 ms (96.38%), Space: 16.5 MB (39.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,13 +1,17 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        l = 0
-        r = int(c**0.5) 
-        while l<=r:
-            total = (l*l + r*r)
-            if total == c:
+        curr = 0
+        a = 0
+        b = int(c ** (0.5))
+        while a <= b:
+            curr = a * a + b * b
+            if curr == c:
                 return True
-            elif total > c:
-                r-=1
+            elif curr < c:
+                a += 1
             else:
-                l+=1
+                b -= 1
         return False
+
+
+
